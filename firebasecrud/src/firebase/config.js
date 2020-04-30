dcd90062a532a266810bb3be429e290dd65811ab
bahrudinrizkypambudi <bahrudinrizky313@gmail.com>
2020-04-30 10:15:30 +0700

    Fixed

---
+++ firebasecrud/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCr8rBWGUa45nU7l2agN_-jmJ_XegZeiyY",
+  authDomain: "react-5903b.firebaseapp.com",
+  databaseURL: "https://react-5903b.firebaseio.com",
+  projectId: "react-5903b",
+  storageBucket: "react-5903b.appspot.com",
+  messagingSenderId: "141745801733",
+  appId: "1:141745801733:web:0c3093dd9f6ac166576a6c",
+  measurementId: "G-YKFXSTGL9Y"
+};
+
+  export default firebaseConfig;
